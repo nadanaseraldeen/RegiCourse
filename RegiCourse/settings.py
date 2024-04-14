aaf4fcefd217df6a927821b01d08b7db4d901cfb
nadanaseraldeen <201081@ppu.edu.ps>
2024-04-14 21:26:15 +0300

    Database design

--- RegiCourse/settings.py
+++ RegiCourse/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'RegiCourse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'regicourse',
+        'USER': 'root',
+        'PASSWORD': '2342002Nada',
     }
 }
 
--- RegiCourse_App/models.py
+++ RegiCourse_App/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Students(models.Model):
+
+    student_Id = models.AutoField(primary_key=True)
+    student_name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=50)
+
+
+class CourseSchedules(models.Model):
+
+    courseSchedule_Id = models.AutoField(primary_key=True)
+    days = models.CharField(max_length=30, null=False)
+    startTime = models.TimeField(null=False)
+    endTime = models.TimeField(null=False)
+    roomNo = models.CharField(max_length=30, null=False)
+
+
+class Courses(models.Model):
+
+    course_code = models.CharField(max_length=20, primary_key=True)
+    course_name = models.CharField(max_length=150, null=False)
+    description = models.CharField(max_length=300, null=False)
+    prerequisites = models.OneToOneField('self', on_delete=models.CASCADE, null=True)
+    instructor_name = models.CharField(max_length=150, null=False)
+    capacity = models.IntegerField(null=False)
+    schedule = models.OneToOneField(CourseSchedules, on_delete=models.CASCADE)
+
+
+class StudentsReg(models.Model):
+
+    studentReg_Id = models.AutoField(primary_key=True)
+    student = models.ForeignKey(Students, on_delete=models.CASCADE)
+    course = models.ForeignKey(Courses, on_delete=models.CASCADE)
+
