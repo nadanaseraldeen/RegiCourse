2c9f9dad18a7a7d55b1939b78d2eebc55172e28c
nadanaseraldeen <201081@ppu.edu.ps>
2024-04-16 01:40:38 +0300

    Signup and Login Backend

--- RegiCourse_App/urls.py
+++ RegiCourse_App/urls.py
@@ -1,10 +1,12 @@
-from django.urls import  path
-
+from django.urls import path
 from RegiCourse_App import views
 
 urlpatterns = [
     path('master/', views.master),
-    path('login/', views.login, name='login'),
-    path('home/', views.home),
+    path('home/', views.home, name='home'),
     path('logout/', views.logout, name='logout'),
+    path('signup-login/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('authenticate/', views.authenticate_user, name='authenticate'),
+
 ]
--- RegiCourse_App/views.py
+++ RegiCourse_App/views.py
@@ -1,21 +1,78 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.contrib import messages
+from RegiCourse_App.models import Students
 
-# Create your views here.
 
 def master(request):
 
     return render(request,'master.html')
 
-def login(request):
-
-    return render(request,'login.html')
 
 def home(request):
 
     return render(request,'home.html')
 
+
 def logout(request):
+
+    return redirect('login')
+
+
+def signup(request):
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirmPassword = request.POST.get('confirmPassword')
+
+        if password != confirmPassword:
+             messages.error(request, 'Passwords do not match')
+             return render(request, 'login.html', {'signup_errors': messages.get_messages(request)})
+
+
+        if User.objects.filter(email=email).exists():
+
+           messages.error(request, 'Email is already in use')
+           return render(request, 'login.html', {'signup_errors': messages.get_messages(request)})
+
+        student = User.objects.create_user(email, email, password)
+        student.name = name
+        student.save()
+
+        students = Students(student_name=name, email=email, password=password)
+        students.save()
+
+        return redirect('home')
+
+    return render(request, 'login.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def authenticate_user(request):
+
+    if request.method == 'POST':
+        email = request.POST.get('login-email')
+        password = request.POST.get('login-password')
+
+        try:
+            user = Students.objects.get(email=email)
+
+            if user.password == password:
+                return redirect('home')
+            else:
+                messages.error(request, 'The email or password is incorrect')
+                return render(request, 'login.html', {'login_errors': messages.get_messages(request)})
+
+        except Students.DoesNotExist:
+            messages.error(request, 'The email or password is incorrect')
+            return render(request, 'login.html', {'login_errors': messages.get_messages(request)})
 
     return redirect('login')
