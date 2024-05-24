37e9f61f7a02dc0e145d548e1d55d202bbfc9384
nadanaseraldeen <201081@ppu.edu.ps>
2024-05-25 00:26:44 +0300

    Fix SSL mode issue

--- RegiCourse/settings.py
+++ RegiCourse/settings.py
@@ -114,7 +114,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=600)
+db_from_env = dj_database_url.config(conn_max_age=600, sslmode='disable')
 DATABASES['default'].update(db_from_env)
 django_heroku.settings(locals())
 
